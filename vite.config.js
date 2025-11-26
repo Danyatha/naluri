71fe421782473e8ed9ab15923727ad693ad13962
Danyatha <dany.atha1803@mail.ugm.ac.id>
2025-11-26 14:33:02 +0700

    push vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        allowedHosts: [
+            'bb4230ae-7bb7-4d09-baab-3db4a0f16f19-00-12d805pgory6v.pike.replit.dev'
+        ],
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
